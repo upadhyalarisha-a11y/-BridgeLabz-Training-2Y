ffdee1539aa397d018af4a7e90d50d3da810a825
larishaupadhyaycsaiml24-ux <larisha.upadhyay_cs.aiml24@gla.ac.in>
2025-08-31 01:30:52 +0530

    [Larisha] Add . Sum of n natural numbers using while loop and compare with formula

---
+++ Programming-Elements/Level-1/SumOfNaturalWhile.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class SumOfNaturalWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        if(n < 1) {
+            System.out.println(n + " is not a natural number");
+        } else {
+            int sumFormula = n*(n+1)/2;
+            int sumLoop = 0, i = 1;
+            while(i <= n) {
+                sumLoop += i;
+                i++;
+            }
+            System.out.println("Sum using formula: " + sumFormula);
+            System.out.println("Sum using while loop: " + sumLoop);
+        }
+        sc.close();
+    }
+}
