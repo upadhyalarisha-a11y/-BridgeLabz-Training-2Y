aeb5c5a83100b1a966f93b03ae4b1eb1210b15db
larishaupadhyaycsaiml24-ux <larisha.upadhyay_cs.aiml24@gla.ac.in>
2025-09-16 21:24:26 +0530

    [Larisha] Add SumNaturalNumbers.java to calculate sum of n natural numbers using loop

---
+++ Java-Methods/Level-2/SumNaturalNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SumNaturalNumber {
+
+    // Method to calculate sum of n natural numbers using loop
+    public static int findSum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        int result = findSum(n);
+
+        System.out.println("Sum of first " + n + " natural numbers is: " + result);
+
+        sc.close();
+    }
+}
