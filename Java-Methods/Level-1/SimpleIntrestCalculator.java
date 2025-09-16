cf8405a43f116a600a8bae755f55605e15c71263
larishaupadhyaycsaiml24-ux <larisha.upadhyay_cs.aiml24@gla.ac.in>
2025-09-16 20:30:29 +0530

    [Larisha] Add SimpleIntrestCalculator.java program to calculate simple interest using method

---
+++ Java-Methods/Level-1/SimpleIntrestCalculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SimpleIntrestCalculator{
+
+    
+    public static double calculateSI(double principal, double rate, double time) {
+        return (principal * rate * time) / 100;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+      
+        System.out.print("Enter Principal: ");
+        double principal = sc.nextDouble();
+
+        System.out.print("Enter Rate of Interest: ");
+        double rate = sc.nextDouble();
+
+        System.out.print("Enter Time (in years): ");
+        double time = sc.nextDouble();
+
+        
+        double si = calculateSI(principal, rate, time);
+
+        // Output
+        System.out.println("The Simple Interest is " + si +
+                " for Principal " + principal +
+                ", Rate of Interest " + rate +
+                " and Time " + time);
+
+        sc.close();
+    }
+}
