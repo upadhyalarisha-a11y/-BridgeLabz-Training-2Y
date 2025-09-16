b29980e5673d68bff68a3a7ab1a917cc44854869
larishaupadhyaycsaiml24-ux <larisha.upadhyay_cs.aiml24@gla.ac.in>
2025-09-16 21:30:02 +0530

    [Larisha] Add QuotientRemainder.java to calculate quotient and remainder

---
+++ Java-Methods/Level-2/QuotientRemainder.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class QuotientRemainder {
+
+    // Method to find quotient and remainder
+    public static int[] findRemainderAndQuotient(int number, int divisor) {
+        int quotient = number / divisor;
+        int remainder = number % divisor;
+        return new int[]{quotient, remainder};
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int number = sc.nextInt();
+
+        System.out.print("Enter divisor: ");
+        int divisor = sc.nextInt();
+
+        if (divisor == 0) {
+            System.out.println("Divisor cannot be zero.");
+        } else {
+            int[] result = findRemainderAndQuotient(number, divisor);
+            System.out.println("Quotient = " + result[0]);
+            System.out.println("Remainder = " + result[1]);
+        }
+
+        sc.close();
+    }
+}
