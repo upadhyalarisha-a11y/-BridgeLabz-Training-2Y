8996bf12337ddcc0a1d4ce1003cd62f0ac5ec8b1
larishaupadhyaycsaiml24-ux <larisha.upadhyay_cs.aiml24@gla.ac.in>
2025-09-17 05:38:17 +0530

    [Larisha] Add . Circle class with area and circumference calculation methods

---
+++ Java-Class-Object/Level-1/Circle.java
@@ -0,0 +1,32 @@
+// File: Circle.java
+public class Circle {
+    // Private attribute
+    private double radius;
+
+    // Constructor
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    // Method to calculate area
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    // Method to calculate circumference
+    public double calculateCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    // Method to display results
+    public void displayDetails() {
+        System.out.println("Radius: " + radius);
+        System.out.println("Area: " + calculateArea());
+        System.out.println("Circumference: " + calculateCircumference());
+    }
+
+    public static void main(String[] args) {
+        Circle c = new Circle(7);
+        c.displayDetails();
+    }
+}
