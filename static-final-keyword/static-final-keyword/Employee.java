6fe5b66441732ad39004256be27bc2b09d91b08b
larishaupadhyaycsaiml24-ux <larisha.upadhyay_cs.aiml24@gla.ac.in>
2025-10-27 11:38:46 +0530

    [Larisha] Add ass some files

---
+++ static-final-keyword/static-final-keyword/Employee.java
@@ -0,0 +1,27 @@
+class Employee {
+    private static String companyName = "Tech Solutions Ltd.";
+    private static int totalEmployees = 0;
+    private final int id;
+    private String name;
+    private String designation;
+
+    public Employee(String name, int id, String designation) {
+        this.name = name;
+        this.id = id;
+        this.designation = designation;
+        totalEmployees++;
+    }
+
+    public static void displayTotalEmployees() {
+        System.out.println("Total Employees: " + totalEmployees);
+    }
+
+    public void displayEmployeeDetails() {
+        if (this instanceof Employee) {
+            System.out.println("Company: " + companyName);
+            System.out.println("Name: " + name);
+            System.out.println("ID: " + id);
+            System.out.println("Designation: " + designation);
+        }
+    }
+}
---
+++ static-final-keyword/static-final-keyword/Student.java
@@ -0,0 +1,27 @@
+class Student {
+    private static String universityName = "National University";
+    private static int totalStudents = 0;
+    private final int rollNumber;
+    private String name;
+    private String grade;
+
+    public Student(String name, int rollNumber, String grade) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.grade = grade;
+        totalStudents++;
+    }
+
+    public static void displayTotalStudents() {
+        System.out.println("Total Students: " + totalStudents);
+    }
+
+    public void displayStudentDetails() {
+        if (this instanceof Student) {
+            System.out.println("University: " + universityName);
+            System.out.println("Name: " + name);
+            System.out.println("Roll Number: " + rollNumber);
+            System.out.println("Grade: " + grade);
+        }
+    }
+}
